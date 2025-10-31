09be90cce6cd71d0076406e60fb479f98bef6b70
yasserA5 <134278379+yasserA5@users.noreply.github.com>
2025-10-31 07:09:45 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const priceMap = {
   business: 7150,
   mobile: 77000,
   advanced: 13200,
-  enterprise: 132000
+  enterprise: 132000,
 };
 
 // Update price dynamically
@@ -24,21 +24,23 @@ app.post("/create-checkout-session", async (req, res) => {
   const { pack } = req.body;
   const amount = priceMap[pack];
 
-  if(!amount) return res.status(400).json({ error: "Invalid pack" });
+  if (!amount) return res.status(400).json({ error: "Invalid pack" });
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
-    line_items: [{
-      price_data: {
-        currency: "usd",
-        product_data: { name: pack + " Pack" },
-        unit_amount: amount
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: { name: pack + " Pack" },
+          unit_amount: amount,
+        },
+        quantity: 1,
       },
-      quantity: 1
-    }],
+    ],
     mode: "payment",
     success_url: "http://localhost:4242/success.html",
-    cancel_url: "http://localhost:4242/cancel.html"
+    cancel_url: "http://localhost:4242/cancel.html",
   });
 
   res.json({ id: session.id });
